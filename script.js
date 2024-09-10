61a4b0d5b699850fae67e759bebf1b02bcf26d77
iampythoncoder <80129110+iampythoncoder@users.noreply.github.com>
2024-09-10 19:41:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    ScrollReveal().reveal('.hero-content', { delay: 300, duration: 1000, origin: 'bottom', distance: '50px' });
+    ScrollReveal().reveal('.category h2', { delay: 300, duration: 1000, origin: 'bottom', distance: '50px' });
+    ScrollReveal().reveal('.category-grid .category-item', { interval: 200, duration: 1000, origin: 'bottom', distance: '50px' });
+});
